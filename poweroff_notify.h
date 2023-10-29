822d9437d195554f8698f664539c91a4b04a291c
Andrey Kuzmin <p.github@prepodam.ru>
2023-10-29 15:28:15 +0300

    Fix led show logic.

--- poweroff_notify.h
+++ poweroff_notify.h
@@ -5,19 +5,21 @@ public:
   PowerOffNotify() { pinMode(SYSTEMLED, OUTPUT); }
 
   void poweroff() {
-    if (_on) {
-      return;
-    }
-
-    digitalWrite(SYSTEMLED, HIGH);
-  }
-
-  void poweron() {
     if (!_on) {
       return;
     }
 
+    digitalWrite(SYSTEMLED, HIGH);
+    _on = false;
+  }
+
+  void poweron() {
+    if (_on) {
+      return;
+    }
+
     digitalWrite(SYSTEMLED, LOW);
+    _on = true;
   }
 
 private:
