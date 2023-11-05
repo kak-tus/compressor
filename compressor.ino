4c1c0ac7f51c11af8ddea7a263391e05a95d9843
Andrey Kuzmin <p.github@prepodam.ru>
2023-11-06 01:55:15 +0300

    Sensor.
    Use lower resistor to calibrate temp sensor.

--- compressor.ino
+++ compressor.ino
@@ -6,16 +6,17 @@
 
 const uint8_t POWEROFFPIN = 2;
 const uint8_t TEMP1PIN = A4;
+const uint8_t MAP1PIN = A0;
 
 const int LOOPDELAY = 10;
 
 TimerMs poweroffCheck(100, 1, 0);
-TimerMs temperatureCheck(500, 1, 0);
+TimerMs logCheck(500, 1, 0);
 
 PowerOff powerOff(POWEROFFPIN);
 PowerOffNotify powerOffNotify;
 
-Sensor sens2(TEMP1PIN);
+Sensor sens1(TEMP1PIN, MAP1PIN);
 
 void setup() { Serial.begin(9600); }
 
@@ -28,9 +29,22 @@ void loop() {
     }
   }
 
-  if (temperatureCheck.tick()) {
+  if (logCheck.tick()) {
     Serial.print(">temperature:");
-    Serial.println(sens2.temperature());
+    Serial.println(sens1.temperature());
+
+    float pressure = sens1.pressure();
+    Serial.print(">pressure (pa):");
+    Serial.println(pressure);
+
+    Serial.print(">pressure (mm):");
+    Serial.println(sens1.pressureInMM());
+
+    Serial.print(">voltage temp:");
+    Serial.println(sens1.voltageTemp());
+
+    Serial.print(">voltage map:");
+    Serial.println(sens1.voltageMap());
   }
 
   delay(LOOPDELAY);
--- sensor.h
+++ sensor.h
@@ -1,14 +1,59 @@
+const float baseVoltage = 5.0;
+const float deltaMAP = 0.37;
+const float angleMAP = 22.643;
+const float kpaToMM = 133.3224;
+const float temp1 = 18.5;
+const float temp2 = 62;
+const float temp1Voltage = 3.43;
+const float temp2Voltage = 1.86;
+
 class Sensor {
 public:
-  Sensor(byte pin) : _pin(pin) {
-    pinMode(_pin, INPUT);
+  Sensor(uint8_t tempPin, uint8_t mapPin) : _tempPin(tempPin), _mapPin(mapPin) {
+    pinMode(_tempPin, INPUT);
+    pinMode(_mapPin, INPUT);
+
+    _deltaTemp =
+        (temp2Voltage * temp1 - temp1Voltage * temp2) / (temp2 - temp1);
+
+    _angleTemp = (temp1 - temp2) / (temp1Voltage - temp2Voltage);
   }
 
-  int temperature() {
-    int temperature = analogRead(_pin);
-    return temperature;
+  float temperature() {
+    int value = analogRead(_tempPin);
+    _voltageTemp = (float)(value * baseVoltage) / 1024;
+
+    float rawTemp = (_voltageTemp + _deltaTemp) * _angleTemp;
+
+    _temp += (rawTemp - _temp) * 0.1;
+
+    return _temp;
   }
+
+  float pressure() {
+    int value = analogRead(_mapPin);
+    _voltageMap = (float)(value * baseVoltage) / 1024;
+
+    float rawPressure = (_voltageMap + deltaMAP) * angleMAP * 1000;
+
+    // Initial
+    if (_pressure < 0.1) {
+      _pressure = rawPressure;
+      return _pressure;
+    }
+
+    _pressure += (rawPressure - _pressure) * 0.1;
+    return _pressure;
+  }
+
+  float pressureInMM() { return _pressure / kpaToMM; }
+  float voltageMap() { return _voltageMap; }
+  float voltageTemp() { return _voltageTemp; }
 
 private:
-  const byte _pin;
+  const uint8_t _tempPin;
+  const uint8_t _mapPin;
+  float _pressure, _temp;
+  float _deltaTemp, _angleTemp;
+  float _voltageMap, _voltageTemp;
 };
