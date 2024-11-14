6f98301ae77a8634ee41926efb1574aac3444a17
Andrey Kuzmin <p.github@prepodam.ru>
2024-11-15 00:14:56 +0300

    Incorrect temp detect fix + new value.

--- sensor.h
+++ sensor.h
@@ -12,10 +12,15 @@ public:
     int16_t voltageFrom, voltageDiff, tempFrom, tempDiff;
 
     // Themperature sensor is not linear, so use this calculation
-    if (_voltageTemp >= 707) {
+    if (_voltageTemp >= 829) {
       voltageFrom = 1024;
-      voltageDiff = voltageFrom - 707;
+      voltageDiff = voltageFrom - 829;
       tempFrom = -40;
+      tempDiff = 1 - tempFrom;
+    } else if (_voltageTemp >= 707) {
+      voltageFrom = 829;
+      voltageDiff = voltageFrom - 707;
+      tempFrom = 1;
       tempDiff = 20 - tempFrom;
     } else if (_voltageTemp >= 670) {
       voltageFrom = 707;
@@ -25,37 +30,37 @@ public:
     } else if (_voltageTemp >= 584) {
       voltageFrom = 670;
       voltageDiff = voltageFrom - 584;
-      tempFrom = 30;
-      tempDiff = 40 - tempFrom;
+      tempFrom = 22;
+      tempDiff = 30 - tempFrom;
     } else if (_voltageTemp >= 512) {
       voltageFrom = 584;
       voltageDiff = voltageFrom - 512;
-      tempFrom = 40;
-      tempDiff = 50 - tempFrom;
+      tempFrom = 30;
+      tempDiff = 40 - tempFrom;
     } else if (_voltageTemp >= 427) {
       voltageFrom = 512;
       voltageDiff = voltageFrom - 427;
-      tempFrom = 50;
-      tempDiff = 60 - tempFrom;
+      tempFrom = 40;
+      tempDiff = 50 - tempFrom;
     } else if (_voltageTemp >= 319) {
       voltageFrom = 427;
       voltageDiff = voltageFrom - 319;
-      tempFrom = 60;
-      tempDiff = 70 - tempFrom;
+      tempFrom = 50;
+      tempDiff = 60 - tempFrom;
     } else if (_voltageTemp >= 290) {
       voltageFrom = 319;
       voltageDiff = voltageFrom - 290;
-      tempFrom = 70;
-      tempDiff = 80 - tempFrom;
+      tempFrom = 60;
+      tempDiff = 70 - tempFrom;
     } else if (_voltageTemp >= 247) {
       voltageFrom = 290;
       voltageDiff = voltageFrom - 247;
-      tempFrom = 80;
-      tempDiff = 90 - tempFrom;
+      tempFrom = 70;
+      tempDiff = 80 - tempFrom;
     } else {
       voltageFrom = 247;
       voltageDiff = voltageFrom - 0;
-      tempFrom = 90;
+      tempFrom = 80;
       tempDiff = 130 - tempFrom;
     }
 
@@ -70,17 +75,9 @@ public:
   uint32_t pressure() {
     _voltageMap = analogRead(_mapPin);
 
-    float rawPressure =
-        (_voltageMap + deltaMAP) * angleMAP * 1000 + _mapCorrection;
+    _pressure = (_voltageMap + deltaMAP) * angleMAP * 1000 + _mapCorrection;
 
-    // Initial
-    if (_pressure < 0.1) {
-      _pressure = rawPressure;
-      return _pressure;
-    }
-
-    _pressure += (rawPressure - _pressure) * 0.5;
-    return uint32_t(_pressure);
+    return _pressure;
   }
 
   uint16_t pressureInMM() { return _pressure / kpaToMM; }
@@ -94,6 +91,7 @@ private:
   // angle = (pressure1 - pressure2) / (voltage1 - voltage2)
   //
   // Voltages must be native arduino integer values (0-1024)
+  // Pressure - is value in kpa
   //
   // nativeVoltage = voltage * 1024 / 5
   const float deltaMAP = 33.29;
@@ -105,7 +103,7 @@ private:
   const uint8_t _mapPin;
 
   float _temp;
-  float _pressure;
+  uint32_t _pressure;
 
   uint16_t _voltageMap, _voltageTemp;
 
