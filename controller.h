7ffbda6238be594ee3d504b1f29eb6e51422c6a6
Andrey Kuzmin <p.github@prepodam.ru>
2025-10-27 00:17:00 +0300

    Use single mode.

--- controller.h
+++ controller.h
@@ -9,19 +9,20 @@ public:
   }
 
   void poweron() {
+    _poweredonAt = millis();
     _compressorBlocked = false;
 
     if (_poweredoffAt != 0 && !timeout(_poweredoffAt, 2000)) {
-      if (_mode == PERFOMANCE) {
-        _mode = NORMAL;
+      if (_mode == NORMAL) {
+        _mode = COMFORT;
         _allowCompressor = false;
 
-        Serial.println("Mode: normal");
+        Serial.println("Mode: comfort");
       } else {
-        _mode = PERFOMANCE;
+        _mode = NORMAL;
         _allowCompressor = true;
 
-        Serial.println("Mode: perfomance");
+        Serial.println("Mode: normal");
       }
     } else {
       _mode = NORMAL;
@@ -32,34 +33,32 @@ public:
   }
 
   void control(uint8_t posMainThrottle) {
-    if (_mode != PERFOMANCE) {
-      if (posMainThrottle < 5) {
-        _allowAt = 0;
+    if (posMainThrottle <= 3) {
+      _allowAt = 0;
 
-        if (_allowCompressor && _disallowAt == 0) {
-          _disallowAt = millis();
-        }
-      } else if (posMainThrottle > 10) {
-        _disallowAt = 0;
-
-        if (!_allowCompressor && _allowAt == 0) {
-          _allowAt = millis();
-        }
+      if (_allowCompressor && _disallowAt == 0) {
+        _disallowAt = millis();
       }
+    } else if (posMainThrottle > 3) {
+      _disallowAt = 0;
 
-      if (_disallowAt != 0 && timeout(_disallowAt, disallowTimeout)) {
-        _allowCompressor = false;
-        _disallowAt = 0;
-        _allowAt = 0;
-
-        Serial.println("Disallow compressor: disallow");
-      } else if (_allowAt != 0 && timeout(_allowAt, allowTimeout)) {
-        _allowCompressor = true;
-        _disallowAt = 0;
-        _allowAt = 0;
-
-        Serial.println("Disallow compressor: allow");
+      if (!_allowCompressor && _allowAt == 0) {
+        _allowAt = millis();
       }
+    }
+
+    if (_disallowAt != 0 && timeout(_disallowAt, disallowTimeout)) {
+      _allowCompressor = false;
+      _disallowAt = 0;
+      _allowAt = 0;
+
+      Serial.println("Disallow compressor: disallow");
+    } else if (_allowAt != 0 && timeout(_allowAt, allowTimeout)) {
+      _allowCompressor = true;
+      _disallowAt = 0;
+      _allowAt = 0;
+
+      Serial.println("Disallow compressor: allow");
     }
   }
 
@@ -72,6 +71,12 @@ public:
   }
 
   uint8_t allowCompressor() {
+    // Don't switch compressor on while power on engine
+    // To fix hight RPM
+    if (!timeout(_poweredonAt, engineOnTimeout)) {
+      return false;
+    }
+
     if (_compressorBlocked) {
       return false;
     }
@@ -92,15 +97,16 @@ private:
   // 80 - is bad, stop boost
   const int16_t boostOffTemperature = 80;
 
-  enum modeType { NORMAL = 0, PERFOMANCE = 1, COMFORT = 2 };
+  enum modeType { COMFORT = 0, NORMAL = 1 };
   modeType _mode = NORMAL;
 
-  unsigned long _poweredoffAt;
+  unsigned long _poweredoffAt, _poweredonAt;
 
   unsigned long _disallowAt, _allowAt;
 
   bool _allowCompressor = true, _compressorBlocked = false;
 
   const uint8_t allowTimeout = 100;
-  const uint8_t disallowTimeout = 1000;
+  const uint16_t disallowTimeout = 1000;
+  const uint16_t engineOnTimeout = 3000;
 };
